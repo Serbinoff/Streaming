e837d9708c53f8171e8c7640e50b6cb6a8df27b8
Serbinoff <serbinoff87@gmail.com>
2019-02-22 13:51:09 +0100

    Initial commit

---
+++ src/main/scala/Streaming.scala
@@ -0,0 +1,69 @@
+import java.util.Properties
+import org.apache.spark.streaming._
+import org.apache.spark.SparkConf
+import scalaj.http.{Http, HttpResponse}
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Future, blocking}
+import scala.util.{Failure, Success}
+
+object Streaming extends App {
+
+    val props = new Properties()
+    props.put("bootstrap.servers", "localhost:9092")
+    props.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer")
+
+    val producer = new KafkaProducer[String, String](props)
+
+    val sparkConf = new SparkConf().setAppName("StreamingJob").setMaster("local[*]")
+    val ssc = new StreamingContext(sparkConf,Seconds(10))
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> "localhost:9092",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "use_a_separate_group_id_for_each_stream",
+      "auto.offset.reset" -> "earliest"
+    )
+
+    val topics = Set("oldKafkaTopic")
+    val messages = KafkaUtils.createDirectStream[String, String](ssc, PreferConsistent,
+      Subscribe[String, String](topics, kafkaParams))
+
+    messages.foreachRDD(rdd => rdd.map(record => Utils.fromJson[KafkaRequest](record.value()))foreach(i =>
+      fetchInfo(CurrencyRequest(i.price, "USD", args(0)))
+        .onComplete {
+          case Success(res) => println(Utils.toJson(KafkaResponse(i.id, res.body.toDouble, args(0))))
+            producer.send(new ProducerRecord("newKafkaTopic", "key", Utils.toJson(KafkaResponse(i.id, res.body.toDouble, args(0)))))
+          case Failure(e) => println(s"Data fetch failed: ${e.getStackTrace}")
+        })
+    )
+
+    ssc.start()
+    ssc.awaitTermination()
+
+    def fetchInfo(currencyRequest: CurrencyRequest): Future[HttpResponse[String]] = {
+      val message = Http("http://localhost:8080/convertor")
+        .postData(
+          s"""{"value": ${currencyRequest.value}, "fromCurrency": "${currencyRequest.fromCurrency}", "toCurrency": "${currencyRequest.toCurrency}"}""")
+        .header("Content-Type", "application/json")
+        .header("Charset", "UTF-8")
+        .option(scalaj.http.HttpOptions.readTimeout(10000)).asString
+      println(s"REST: $message")
+      Future {
+        blocking {
+          message
+        }
+      }
+    }
+}
+
+case class KafkaRequest(id: Int, price: Int)
+case class KafkaResponse(id: Int, body: Double, str: String)
+case class CurrencyRequest(value: Double, fromCurrency: String, toCurrency: String)
+case class CurrencyResponse(price: Int, currency: String)
